06a900bfee3a655794458e9f8bc51b5d0a87f417
Ido <ido.hadary1@gmail.com>
2024-04-03 15:47:05 +0300

    Update project
    Arrangement, change and correction of classes and functions.

--- SoftwareQA_Final_Project/src/test/java/selleniun_project/Password_validation.java
+++ SoftwareQA_Final_Project/src/test/java/selleniun_project/Password_validation.java
@@ -25,7 +25,7 @@ public class Password_validation {
 	@Test
 	public void FP_Q2_TC1_validPassword() {
 		textBox = browser.findElement(By.name("characters"));
-		textBox.sendKeys("idohad1");
+		textBox.sendKeys("iDohaD1");
 		browser.findElement(By.name("validate")).click();
 		
 		String expected = "Valid Value";
@@ -35,7 +35,7 @@ public class Password_validation {
 	}
 	
 	@Test
-	public void FP_Q2_TC2_invalidPassword() {
+	public void FP_Q2_TC2_invalidPasswordCharacter() {
 		textBox = browser.findElement(By.name("characters"));
 		textBox.sendKeys("idohad$");
 		browser.findElement(By.name("validate")).click();
@@ -49,7 +49,7 @@ public class Password_validation {
 	@Test
 	public void FP_Q2_TC3_validPassword() {
 		textBox = browser.findElement(By.name("characters"));
-		textBox.sendKeys("idohad*");
+		textBox.sendKeys("1do*h7d");
 		browser.findElement(By.name("validate")).click();
 		
 		String expected = "Valid Value";
@@ -59,7 +59,43 @@ public class Password_validation {
 	}
 	
 	@Test
-	public void FP_Q2_TC4_invalidShortPassword() {
+	public void FP_Q2_TC4_invalidPasswordCharacter() {
+		textBox = browser.findElement(By.name("characters"));
+		textBox.sendKeys("idaNI@*");
+		browser.findElement(By.name("validate")).click();
+		
+		String expected = "Invalid Value";
+		String actual = browser.findElement(By.name("validation_message")).getAttribute("value");
+		assertEquals(expected, actual);
+		browser.close();
+	}
+	
+	@Test
+	public void FP_Q2_TC5_validPassword() {
+		textBox = browser.findElement(By.name("characters"));
+		textBox.sendKeys("DaN5*Va");
+		browser.findElement(By.name("validate")).click();
+		
+		String expected = "Valid Value";
+		String actual = browser.findElement(By.name("validation_message")).getAttribute("value");
+		assertEquals(expected, actual);
+		browser.close();
+	}
+	
+	@Test
+	public void FP_Q2_TC6_invalidPasswordCharacter() {
+		textBox = browser.findElement(By.name("characters"));
+		textBox.sendKeys("V*aK*1&");
+		browser.findElement(By.name("validate")).click();
+		
+		String expected = "Invalid Value";
+		String actual = browser.findElement(By.name("validation_message")).getAttribute("value");
+		assertEquals(expected, actual);
+		browser.close();
+	}
+	
+	@Test
+	public void FP_Q2_TC7_invalidShortPassword() {
 		textBox = browser.findElement(By.name("characters"));
 		textBox.sendKeys("hada");
 		browser.findElement(By.name("validate")).click();
@@ -71,9 +107,9 @@ public class Password_validation {
 	}
 	
 	@Test
-	public void FP_Q2_TC5_invalidLongPassword() {
+	public void FP_Q2_TC8_invalidLongPassword() {
 		textBox = browser.findElement(By.name("characters"));
-		textBox.sendKeys("ido123had1");
+		textBox.sendKeys("iDo12*Dani1");
 		browser.findElement(By.name("validate")).click();
 		
 		String expected = "Invalid Value";
--- SoftwareQA_Final_Project/src/test/java/selleniun_project/TestSuite.java
+++ SoftwareQA_Final_Project/src/test/java/selleniun_project/TestSuite.java
@@ -6,7 +6,9 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
-	Triangle_classification.class
+	Triangle_classification.class, 
+	Password_validation.class, 
+	Selenium_calculator.class
 })
 public class TestSuite {
 	
